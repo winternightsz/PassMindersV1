fd144de4f86570c5fd48b4ab7e4224a59e9c525e
Izabela <104799350+winternightsz@users.noreply.github.com>
2024-09-29 00:29:32 +0000

    Pagina Cadastro e Login atualizadas

--- frontend/src/components/CadastroForm.js
+++ frontend/src/components/CadastroForm.js
@@ -1,121 +1,193 @@
-const CadastroForm = () => {
-    return (
-      <>
-        <div className="flex items-center justify-center min-h-screen bg-azul60">
-          <div className="w-full max-w-2xl bg-branco30 rounded-3xl shadow-lg p-8 md:p-16">
-            <div className="space-y-6">
-              {/* Título */}
-              <h1 className="text-4xl xl:text-6xl font-bold text-azul10 border-b-4 border-azul10 pb-2">
-                Criar uma conta
-              </h1>
-              {/* Formulário */}
-              <form className="space-y-6">
-                {/* Campo de E-mail */}
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block mb-2 text-base font-medium text-azul10"
-                  >
-                    E-mail
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="!bg-azul60 border border-gray-300 text-azul10 text-base rounded-lg !focus:ring-azul10 !focus:border-azul10 block w-full p-2.5"
-                    placeholder="email@exemplo.com"
-                    required
-                  />
-                </div>
-                {/* Campo de Nome de Usuário */}
-                <div>
-                  <label
-                    htmlFor="nome"
-                    className="block mb-2 text-base font-medium text-azul10"
-                  >
-                    Nome de Usuário
-                  </label>
-                  <input
-                    type="text"
-                    name="nome"
-                    id="nome"
-                    className="!bg-azul60 border border-gray-300 text-azul10 text-base rounded-lg focus:ring-azul10 focus:border-azul10 block w-full p-2.5"
-                    placeholder="@username"
-                    required
-                  />
-                </div>
-                {/* Campo de Senha */}
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block mb-2 text-base font-medium text-azul10"
-                  >
-                    Senha
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="********"
-                    className="!bg-azul60 border border-gray-300 text-azul10 text-base rounded-lg focus:ring-azul10 focus:border-azul10 block w-full p-2.5"
-                    required
-                  />
-                </div>
-                {/* Campo de Confirmar Senha */}
-                <div>
-                  <label
-                    htmlFor="confirm-password"
-                    className="block mb-2 text-base font-medium text-azul10"
-                  >
-                    Confirmar Senha
-                  </label>
-                  <input
-                    type="password"
-                    name="confirm-password"
-                    id="confirm-password"
-                    placeholder="********"
-                    className="!bg-azul60 border border-gray-300 text-azul10 text-base rounded-lg focus:ring-azul10 focus:border-azul10 block w-full p-2.5"
-                    required
-                  />
-                </div>
-                <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                        <input id="terms" 
-                        aria-describedby="terms" 
-                        type="checkbox" 
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 !focus:ring-azul10" required="" />
-                    </div>
-                    <div className="ml-3 text-sm">
-                        <label for="terms" className="font-light text-gray-500 dark:text-gray-300">
-                            Aceito os  
-                            <a className="font-medium text-primary-600 hover:underline" href="#"> Termos e Condições </a>
-                        </label>
-                    </div>
-                </div>
-                {/* Botão de Inscrição */}
-                <button
-                    type="submit"
-                    className="text-branco30 !bg-azul10 hover:bg-azul60 rounded-3xl px-16 py-3 text-center font-medium text-xl mx-auto block">
-                        INSCREVA-SE
-                </button>
+"use client";
+import { useState } from "react";
+import { useRouter } from 'next/navigation';
 
-                {/* Link para Login */}
-                <p className="text-center text-base text-azul60">
-                  ou{" "}
-                  <a
-                    href="/login"
-                    className="text-azul10 hover:underline"
-                  >
-                    Fazer login
-                  </a>
-                </p>
-              </form>
-            </div>
-          </div>
-        </div>
-      </>
-    );
+const CadastroForm = () => {
+  const [email, setEmail] = useState("");
+  const [nome, setNome] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const router = useRouter();
+
+  // funcao que valida a senha
+  const isPasswordValid = (password) => {
+    // Tem que ter pelo menos 8 caracteres, um numero e uma letra maiuscula
+    return /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password);
   };
-  
-  export default CadastroForm;
-  
+
+  // funcao para verificar se tem email e nome de usuario duplicados
+  const checkDuplicateUser = async (email, nome) => {
+    try {
+      const response = await fetch(`http://localhost:5000/api/check-user`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, nome }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        return data.exists; // retorna `true` se existe e `false` se nao tem
+      } else {
+        console.error('Erro ao verificar usuario:', response.status);
+        return true; // quando da erro nao faz o cadastro
+      }
+    } catch (err) {
+      console.error('Erro na requisicao de verificacao:', err);
+      return true; 
+    }
+  };
+
+  // funcao que faz a requisicao para o backend e manipula a submissao do formulario
+  // pra criar um novo usuario no banco de dados
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // validacao de senha e confirmacao
+    if (password !== confirmPassword) {
+      alert("As senhas nao sao iguais.");
+      return;
+    }
+
+    // verifica se a senha atende os requisitos
+    if (!isPasswordValid(password)) {
+      alert("A senha deve ter pelo menos 8 caracteres, incluindo uma letra maiuscula e um numero.");
+      return;
+    }
+
+    // verifica se email ou nome de usuario ja estao cadastrados
+    const isDuplicate = await checkDuplicateUser(email, nome);
+    if (isDuplicate) {
+      alert("Email ou nome de usuario ja estao cadastrados.");
+      return; // impede o cadastro se ja existe
+    }
+
+    // dados para serem enviados para o backend
+    const formData = { email, nome, password };
+
+    try {
+      const response = await fetch('http://localhost:5000/api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Cadastro bem-sucedido:', data);
+        router.push('/login');
+      } else {
+        const error = await response.json();
+        console.error('Erro ao cadastrar:', error);
+      }
+    } catch (err) {
+      console.error('Erro na requisicao:', err);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-azul60">
+      <div className="w-full max-w-2xl bg-branco30 rounded-3xl shadow-lg p-8 md:p-16">
+        <div className="space-y-6">
+          <h1 className="text-4xl xl:text-6xl font-bold text-azul10 border-b-4 border-azul10 pb-2">
+            Criar uma conta
+          </h1>
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email" className="block mb-2 text-base font-medium text-azul10">
+                E-mail
+              </label>
+              <input
+                type="email"
+                name="email"
+                className="!bg-azul60 border border-gray-300 text-azul10 text-base rounded-lg !focus:ring-azul10 !focus:border-azul10 block w-full p-2.5"
+                placeholder="email@exemplo.com"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="nome" className="block mb-2 text-base font-medium text-azul10">
+                Nome de Usuário
+              </label>
+              <input
+                type="text"
+                name="nome"
+                className="!bg-azul60 border border-gray-300 text-azul10 text-base rounded-lg focus:ring-azul10 focus:border-azul10 block w-full p-2.5"
+                placeholder="@username"
+                required
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="block mb-2 text-base font-medium text-azul10">
+                Senha
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="********"
+                className="!bg-azul60 border border-gray-300 text-azul10 text-base rounded-lg focus:ring-azul10 focus:border-azul10 block w-full p-2.5"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="confirm-password" className="block mb-2 text-base font-medium text-azul10">
+                Confirmar Senha
+              </label>
+              <input
+                type="password"
+                name="confirm-password"
+                placeholder="********"
+                className="!bg-azul60 border border-gray-300 text-azul10 text-base rounded-lg focus:ring-azul10 focus:border-azul10 block w-full p-2.5"
+                required
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+            <div className="flex items-start">
+              <div className="flex items-center h-5">
+                <input
+                  id="terms"
+                  aria-describedby="terms"
+                  type="checkbox"
+                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 !focus:ring-azul10"
+                  required
+                />
+              </div>
+              <div className="ml-3 text-sm">
+                <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">
+                  Aceito os{" "}
+                  <a className="font-medium text-primary-600 hover:underline" href="#">
+                    Termos e Condições
+                  </a>
+                </label>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="text-branco30 !bg-azul10 hover:bg-azul60 rounded-3xl px-16 py-3 text-center font-medium text-xl mx-auto block"
+            >
+              INSCREVA-SE
+            </button>
+            <p className="text-center text-base text-azul60">
+              ou{" "}
+              <a href="/login" className="text-azul10 hover:underline">
+                Fazer login
+              </a>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CadastroForm;
--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,78 +1,129 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from 'next/navigation'; //para usar o App Router
+
 const LoginForm = () => {
-    return (
-      <>
-        <div className="flex items-center  justify-center max-w-2xl bg-azul60">
-          <div className="w-full max-w-2xl bg-branco30 rounded-3xl shadow-lg p-8 md:p-12">
-            <div className="space-y-6">
-              {/* Título */}
-              <h1 className="text-4xl font-extrabold text-azul10 border-b-4 border-azul10 pb-2">
-                Login
-              </h1>
-              {/* Formulário */}
-              <form className="space-y-6">
-                {/* Campo de E-mail */}
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block mb-2 text-base font-medium text-azul10"
-                  >
-                    Seu e-mail ou Nome de Usuário
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="!bg-azul60 border border-gray-300 text-azul10 rounded-lg focus:ring-azul10 focus:border-azul10 block w-full p-2.5"
-                    placeholder="email@exemplo.com ou @username"
-                    required
-                  />
-                </div>
-                {/* Campo de Senha */}
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block mb-2 text-base font-medium text-azul10"
-                  >
-                    Senha
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="••••••••"
-                    className="!bg-azul60 border border-gray-300 text-azul10 rounded-lg focus:ring-azul10 focus:border-azul10 block w-full p-2.5"
-                    required
-                  />
-                </div>
-                {/* Lembrar-me e Esqueceu a senha */}
-                <div className="flex items-center justify-between">
-                  <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                      <input
-                        id="remember"
-                        aria-describedby="remember"
-                        type="checkbox"
-                        className="w-4 h-4 border border-gray-300 rounded bg-azul60 focus:ring-3 focus:ring-azul10"
-                        required
-                      />
-                    </div>
-                    <div className="ml-3 text-sm">
-                      <label htmlFor="remember" className="text-azul10">
-                        Lembrar-me
-                      </label>
-                    </div>
-                  </div>
-                  
-                </div>
-                {/* Botão de Sign In */}
-                <button
-                  type="submit"
-                  className="text-branco30 !bg-azul10 hover:bg-azul60 rounded-3xl px-24 py-3 text-center font-medium text-xl mx-auto block"
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const router = useRouter(); // Inicializa o roteador para navegação
+
+  // Função para manipular a submissão do formulário
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = {
+      email,
+      password
+    };
+
+    try {
+      const response = await fetch('http://localhost:5000/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        const token = data.token; // Supondo que o token JWT seja retornado como 'token'
+
+        // Armazenar o token no localStorage
+        localStorage.setItem('authToken', token);
+
+        // Redirecionar para a página principal após login bem-sucedido
+        router.push('/main'); // Redireciona para a rota '/main'
+      } else {
+        // Lidar com erros de autenticação
+        const error = await response.json();
+        setErrorMessage(error.message || 'Erro ao fazer login');
+      }
+    } catch (err) {
+      console.error('Erro na requisição:', err);
+      setErrorMessage('Erro ao fazer login. Tente novamente.');
+    }
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-center max-w-2xl bg-azul60">
+        <div className="w-full max-w-2xl bg-branco30 rounded-3xl shadow-lg p-8 md:p-12">
+          <div className="space-y-6">
+            {/* Título */}
+            <h1 className="text-4xl font-extrabold text-azul10 border-b-4 border-azul10 pb-2">
+              Login
+            </h1>
+            {/* Mensagem de erro */}
+            {errorMessage && (
+              <p className="text-red-500 text-sm">{errorMessage}</p>
+            )}
+            {/* Formulário */}
+            <form className="space-y-6" onSubmit={handleSubmit}>
+              {/* Campo de E-mail */}
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-base font-medium text-azul10"
                 >
-                  Fazer Login
-                </button>
-                {/* Link para Inscrever-se */}
-                <div className="flex items-center flex-col">
+                  Seu e-mail ou Nome de Usuário
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  className="!bg-azul60 border border-gray-300 text-azul10 rounded-lg focus:ring-azul10 focus:border-azul10 block w-full p-2.5"
+                  placeholder="email@exemplo.com ou @username"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              {/* Campo de Senha */}
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-base font-medium text-azul10"
+                >
+                  Senha
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="••••••••"
+                  className="!bg-azul60 border border-gray-300 text-azul10 rounded-lg focus:ring-azul10 focus:border-azul10 block w-full p-2.5"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              {/* Lembrar-me */}
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      aria-describedby="remember"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-azul60 focus:ring-3 focus:ring-azul10"
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label htmlFor="remember" className="text-azul10">
+                      Lembrar-me
+                    </label>
+                  </div>
+                </div>
+              </div>
+              {/* Botão de Sign In */}
+              <button
+                type="submit"
+                className="text-branco30 !bg-azul10 hover:bg-azul60 rounded-3xl px-24 py-3 text-center font-medium text-xl mx-auto block"
+              >
+                Fazer Login
+              </button>
+              {/* Link para Inscrever-se */}
+              <div className="flex items-center flex-col">
                 <p className="text-center text-base text-azul10">
                   ou{" "}
                   <a
@@ -83,19 +134,18 @@ const LoginForm = () => {
                   </a>
                 </p>
                 <a
-                    href="#"
-                    className="text-sm font-medium text-gray-400 hover:underline"
-                  >
-                    Esqueceu a senha?
-                  </a>
-                  </div>
-              </form>
-            </div>
+                  href="#"
+                  className="text-sm font-medium text-gray-400 hover:underline"
+                >
+                  Esqueceu a senha?
+                </a>
+              </div>
+            </form>
           </div>
         </div>
-      </>
-    );
-  };
-  
-  export default LoginForm;
-  
+      </div>
+    </>
+  );
+};
+
+export default LoginForm;
