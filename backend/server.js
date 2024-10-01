75b71ee2001217a4282cfe8192e0694a04f3e79c
Flavia Schnaider <schflavia@hotmail.com>
2024-10-01 08:04:33 -0300

    Conexão com Banco Criada

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,18 @@
-// tsc server.ts 
-// node server.ts
-
-import * as mysql from 'mysql2';
-
-const connection = mysql.createConnection ({
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var mysql = require("mysql2");
+var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: '12345',
     database: 'passMinders'
 });
-
-connection.connect((err) => {
+connection.connect(function (err) {
     if (err) {
         console.error('Erro ao conectar no banco de dados: ', err);
         return;
     }
     console.log('Conexão com o banco de dados estabelecida!');
 });
-
 connection.end();
