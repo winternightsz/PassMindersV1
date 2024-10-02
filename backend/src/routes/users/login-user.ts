15c4e035e69c3c429757cc40673dcaa7888947ba
GremaBR <gremabr@gmail.com>
2024-10-02 15:24:44 -0300

    Add login

--- backend/src/routes/users/login-user.ts
+++ backend/src/routes/users/login-user.ts
@@ -1,11 +1,29 @@
 import fastify, { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { knex } from '../../database'
+import { z } from 'zod'
 
 export const LoginUser = async (app: FastifyInstance) =>{
-    const nomeUsuario = "Gregori"
-    const senha = "12345"
-    app.get('/loginUser', async() =>{
-        const tables = await knex('Usuario').select("*").where({nomeUsuario}).where({senha})
-        return tables
+    app.post('/loginUser', async(request, reply) =>{
+        const schema = z.object({
+            nomeUsuario: z.string(),
+            email: z.string().email(),
+            senha: z.string()
+        })
+
+        try{
+            const {nomeUsuario,email,senha} = schema.parse(request.body)
+
+            const user = await knex('Usuario')
+                .where({nomeUsuario,email,senha})
+                .first()
+
+            if(!user){
+                return reply.status(401).send({error: 'Credenciais inválidas'})
+            }
+            
+            return reply.status(200).send({messege: 'Login efetuado com sucesso',user})
+        }catch(error){
+            return reply.status(400).send({ error: error.errors || 'Erro ao fazer login' })
+        }
     })
 }
